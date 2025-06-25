8502808292c77eac407dad6df186161230794785
Bharaths73 <130239394+Bharaths73@users.noreply.github.com>
2025-06-25 19:34:54 +0530

    changed file import name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './app.css';
+import "./App.css";
 import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './Redux/Store.jsx';
